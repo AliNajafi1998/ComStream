a72ff875ae56a4e69cc445f37b375da7dd46ece9
Ali Najafi <najafi1998ali@gmail.com>
2020-08-14 21:27:33 +0430

    Other codes are done now

--- KMeans/main.py
+++ KMeans/main.py
@@ -5,19 +5,19 @@ import os
 
 os.chdir('..')
 path = os.getcwd()
-df = pd.read_pickle(os.path.join(path, 'Data/data_cleaned.pkl')).head(10000)
+df = pd.read_pickle(os.path.join(path, 'Data/reuters_cleaned.pkl'))
 os.chdir('./KMeans')
 
 docs = []
 for i in range(len(df)):
-    docs.append(df.iloc[[i]]['text'].values[0])
+    docs.append(df.iloc[[i]]['TEXT'].values[0])
 
 vectorizer = TfidfVectorizer(min_df=1)
 X = vectorizer.fit_transform(docs)
 
-kmeans = KMeans(10).fit(X)
+kmeans = KMeans(50).fit(X)
 
-for i in range(10000):
+for i in range(len(df)):
     file_name = kmeans.predict(X[i])[0]
     with open(os.path.join(os.getcwd(), f'output/{file_name}.txt'), 'a') as f:
         f.write(docs[i])
