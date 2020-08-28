a0c8eca9890d467775a250fa862e3ce8255576e7
Araz .G Shilabin <arazg999@gmail.com>
2020-08-28 19:37:43 +0430

    updated topic extraction from agents

--- TopicEvaluation/topic_evaluation.py
+++ TopicEvaluation/topic_evaluation.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import spacy
 import pytextrank
 import pandas as pd
-import numpy as np
 import gensim
+from datetime import datetime
 from gensim import corpora, models
 
 
@@ -16,7 +16,9 @@ def get_pred_data(out_dirs, top_n):
     days_list = []
     days_agent_texts = []  # list (day) of, list (size_agent, text_agent)
     for out_dir in os.listdir(out_dirs):
-        days_list.append(out_dir[1:11])
+        if out_dir in ['X2012-05-05 16_27_00--22154', 'X2012-05-05 16_42_00--32634']:
+            continue
+        days_list.append(out_dir)
         one_day_dir = os.path.join(out_dirs, out_dir, 'clusters')
         texts = []
         for label_name in os.listdir(one_day_dir):
@@ -28,6 +30,7 @@ def get_pred_data(out_dirs, top_n):
                 if line.strip() == '':
                     continue
                 text += line.strip() + '. '
+                # text += line.strip() + ' '
                 count += 1
             texts.append((count, text.strip(), label_name))
         texts.sort(reverse=True)
@@ -40,7 +43,22 @@ def get_pred_data(out_dirs, top_n):
     return days_agent_texts, days_list
 
 
-def get_topic(text, top_n):
+def get_topics_top_tf(text2, top_n):
+    text2 = text2.replace(".", "")
+    word2count = {}
+    for word in text2.split():
+        word2count[word] = word2count.get(word, 0) + 1
+
+    keywords2 = set({})
+    for word, count in sorted(word2count.items(), key=lambda item: item[1], reverse=True):
+        keywords2.add(word)
+        if len(keywords2) == top_n:
+            break
+    return keywords2
+    # return ranks
+
+
+def get_topics_textrank(text2, top_n):
     # load a spaCy model, depending on language, scale, etc.
     nlp = spacy.load("en_core_web_sm")
 
@@ -48,49 +66,54 @@ def get_topic(text, top_n):
     tr = pytextrank.TextRank()
     nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
 
-    doc = nlp(text)
+    doc = nlp(text2)
 
     # examine the top-ranked phrases in the document
     ranks = []
-    keywords = set({})
+    keywords2 = set({})
     # for sent in doc._.textrank.summary(limit_phrases=15, limit_sentences=5):
     #     print(sent, end='***\n\n')
     for p in doc._.phrases:
         # print("{:.4f} {:5d}  {}".format(p.rank, p.count, p.text))
         ranks.append((p.rank, p.count, p.text))
         for word in p.text.split():
-            keywords.add(word)
-            if len(keywords) == top_n:
+            keywords2.add(word)
+            if len(keywords2) == top_n:
                 break
-        if len(keywords) == top_n:
+        if len(keywords2) == top_n:
             break
         # if len(ranks) == top_n:
         #     break
-    return keywords
+    return keywords2
     # return ranks
 
 
-def LDA_IDF(num_topics):
+def LDA_IDF(num_topics, max_keywords, time_range=15):
     os.chdir('..')
     path = os.getcwd()
-    df = pd.read_pickle(os.path.join(path, 'Data/data_cleaned_1000k.pkl'))
+    df = pd.read_pickle(os.path.join(path, 'Data/FA/FACUP.pkl'))
+    # reformat_time
+    df['created_at'] = df['created_at'].apply(lambda s: s[11:17] + '00')
+    # df['created_at'] = (pd.to_datetime(df['created_at'].str.strip(), format='%H:%M:%S'))
+    unique_times = df['created_at'].unique()
+    # format of dic_time2texts: {'16:00:00' : ['hi i am x', 'my eyes hurt,...]}
+    dic_time2texts = {time: [] for time in unique_times}
+    for index, row in df.iterrows():
+        dic_time2texts[row['created_at']].append(row['text'])
 
-    date2inds = {}  # date to list of tweet inds in the df
-    for i in range(len(df)):
-        date = df.iloc[i]['created_at']
-        date = date[:10]
-        if date not in date2inds:
-            date2inds[date] = []
-        date2inds[date].append(i)
     days_topics_keywords = []
-    for date, inds in sorted(date2inds.items(), key=lambda x: x[1]):
-        df2 = df.iloc[inds]
-        processed_docs = pd.Series(dtype=object)
-        for i in range(len(df2)):
-            tokens = df2.iloc[i]['text'].split()
-            tokens = list(filter(lambda x: x != "", tokens))
-            processed_docs.at[i] = tokens
+    # will look at the next time_range times to find the topics for that specific time
+    for ind, time in enumerate(sorted(unique_times)):
+        print(ind, time)
 
+        processed_docs = pd.Series(dtype=object)
+        for ind2, (time2, texts) in enumerate(sorted(dic_time2texts.items())):
+            if ind2 >= ind and ind2 < ind + time_range:  # we will only continue if ind2 is in range ind, ind+time_range
+                for text in texts:
+                    tokens = text.strip().split()
+                    tokens = list(filter(lambda x: x != "", tokens))
+                    processed_docs.at[ind2 - ind] = tokens
+        # do lda for this day
         dictionary = gensim.corpora.Dictionary(processed_docs)
         bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
 
@@ -105,54 +128,77 @@ def LDA_IDF(num_topics):
             for ind2, keyword in enumerate(topic.split('"')):
                 if ind2 % 2 == 1:
                     keywords.append(keyword)
+                    if len(keywords) == max_keywords:
+                        break
             topics_keywords.append(keywords)
-        days_topics_keywords.append(topics_keywords)
-        return topics_keywords
+        days_topics_keywords.append((time, topics_keywords))
+        # return topics_keywords
     return days_topics_keywords
-    #
-    # count = 0
-    # for dp in processed_docs:
-    #     unseen_document = dp
-    #     bow_vector = dictionary.doc2bow(unseen_document)
-    #     index = sorted(lda_model_tfidf[bow_vector], key=lambda tup: -1 * tup[1])[0][0]
-    #     # for index, score in sorted(lda_model_tfidf[bow_vector], key=lambda tup: -1 * tup[1]):
-    #     pattern = r"\"[a-z0-9]+\""
-    #     topic_name = re.findall(pattern, lda_model_tfidf.print_topic(index, 1))[0][1:-1]
-    #     with open(os.path.join(os.getcwd(), f'output/{topic_name}.txt'), 'a') as f:
-    #         count += 1
-    #         f.write(' '.join(dp))
-    #         f.write('\n\n')
-    # print(count)
+
+
+def save_lda_results(days_topics_keywords):
+    #  days_topics_keywords = tuple (str day, list of list [[topic1_key1,topic1_key2], [topic2_key1,topic2_key2]])
+    # '5_5_2012_16_1.txt'
+    # print(len(days_topics_keywords))
+    print('saving in file started')
+    for time, topics_keywords in days_topics_keywords:
+        topic_keywords_str = ''
+        for topic_keywords in topics_keywords:
+            for keyword in topic_keywords:
+                topic_keywords_str += keyword + ' '
+            topic_keywords_str = topic_keywords_str.strip()
+            topic_keywords_str += '\n'
+        topic_name = str(time)[:2] + '_' + (str(time)[4:5] if str(time)[3] == '0' else str(time)[3:5])
+        with open(os.path.join(os.getcwd(), f'Data/outputs/LDA_topics/5_5_2012_{topic_name}.txt'), 'a') as f:
+            f.write(topic_keywords_str)
 
 
 if __name__ == '__main__':
+    time_range = 1
+    no_keywords = 4
+    no_topics = 25
     multiagent_days_topics_keywords = []
     out_dirs = os.path.join(Path(os.getcwd()).parent, 'Data/outputs/multi_agent')
-    data, days_list = get_pred_data(out_dirs=out_dirs, top_n=10)  # list (day) of, list (size_agent, text_agent)
+    # list (day) of, list (size_agent, text_agent, label_file_name)
+    data, days_list = get_pred_data(out_dirs=out_dirs, top_n=no_topics)
+    print('got the data')
     for day_ind, texts in enumerate(data):
         topics_keywords = []
+
+        ########################## do the time stuff
+        time = days_list[day_ind]
+        dt = str(time)[12:20]
+        date_time_obj = datetime.strptime(dt, '%H_%M_%S')
+        date_time_obj -= pd.Timedelta(seconds=time_range * 60)
+        topic_name = str(date_time_obj)[11:13] + '_' + str(date_time_obj)[14:16]
+        if topic_name[-2] == '0':
+            topic_name = topic_name[:len(topic_name) - 2] + topic_name[-1]
+
+        # print(str(date_time_obj))
+        list_dates_to_save = ['16_16', '16_26', '16_41', '16_53', '17_1', '17_3', '17_18', '17_24', '17_25', '17_36',
+                              '17_46', '17_56', '18_9']
+        if topic_name not in list_dates_to_save:
+            continue
+        print(topic_name)
+
+        ############################### get labels
         for count, text, label_name in texts:
-            # ranks = get_topics(text, top_n=5)
-            # print(days_list[day_ind], label_name)
-            # for p in ranks:
-            #     print("{:.4f} {:5d}  {}".format(p[0], p[1], p[2]))
-            key_words = get_topic(text, top_n=5)
+            # key_words = get_topics_textrank(text, top_n=no_keywords)
+            key_words = get_topics_top_tf(text, top_n=no_keywords)
             keywords = list(key_words)
-            multiagent_days_topics_keywords.append(keywords)
-            # break
-        break
+            topics_keywords.append(keywords)
+        multiagent_days_topics_keywords.append(topics_keywords)
+        topics_keywords_str = ''
+        for keywords in topics_keywords:
+            for keyword in keywords:
+                topics_keywords_str += keyword + ' '
+            topics_keywords_str = topics_keywords_str.strip()
+            topics_keywords_str += '\n'
 
-    lda_days_topics_keywords = LDA_IDF(num_topics=5)
+        with open(os.path.join(Path(os.getcwd()).parent, f'Data/outputs/multi_agent_topics/5_5_2012_{topic_name}.txt'),
+                  'a') as f:
+            f.write(topics_keywords_str)
 
-    truth = ['state', 'numbers', 'united', 'states', 'leader', 'infected', 'die', 'selfish', 'idiots']
-
-    for topic in multiagent_days_topics_keywords:
-        for keyword in topic:
-            if keyword in truth:
-                print(keyword)
-
-    print('nextttttttt')
-    for topic in lda_days_topics_keywords:
-        for keyword in topic:
-            if keyword in truth:
-                print(keyword)
+    # # returns tuple (str day, list of list [[topic1_key1,topic1_key2], [topic2_key1,topic2_key2]])
+    # lda_days_topics_keywords = LDA_IDF(num_topics=20, max_keywords=5, time_range=time_range)
+    # save_lda_results(lda_days_topics_keywords)
