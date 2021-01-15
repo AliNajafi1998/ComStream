39823bd451683fe951f7e4f1026d1ff726fcdeea
AliNajafi <najafi1998ali@gmail.com>
2021-01-15 18:57:19 +0330

    distace metrics are added

---
+++ ComStream/labse/Utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def get_seconds(time: str):
+    """
+    calculates the seconds of a time
+    :param time: the time as str 'hh:mm:ss'
+    :return: returns time as seconds
+    """
+    s = time.split(':')
+    seconds = float(s[2]) + float(s[1]) * 60 + float(s[0]) * 3600
+    return seconds
+
+
+def get_distance_cosine(coordinator, vec_1, vec_2):
+    return (np.dot(vec_1, vec_2.T)) / (np.linalg.norm(vec_1) * np.linalg.norm(vec_2))
+
+
+def get_distance_euclidean(coordinator, vec_1, vec_2):
+    return np.sqrt(np.sum((vec_1 - vec_2) ** 2))
