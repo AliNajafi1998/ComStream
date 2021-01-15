3ef80a109a9725209c4ffd081beee60c8cf90bd4
Araz .G Shilabin <arazg999@gmail.com>
2021-01-15 23:23:29 +0330

    fixed some importin + file directory bugs

--- ComStream/labse/main.py
+++ ComStream/labse/main.py
@@ -1,20 +1,25 @@
-from .Coordinator import Coordinator
-from .Utils import get_distance_cosine
+from ComStream.ComStream.labse.Coordinator import Coordinator
+from ComStream.ComStream.labse.Utils import get_distance_cosine
 import os
+from pathlib import Path
 
 if __name__ == '__main__':
-    data_path = os.path.join('/ComStream/ComStream/Data/data_cleaned.pkl')  # The file must be pickle file :)
-    embedding_file_path = os.path.join("embeds.npy")
+    p = Path(__file__).parents[2]
+    os.chdir(p)
+    print(os.getcwd())
+    data_path = os.path.join('Data/data_cleaned.pkl')  # The file must be pickle file :)
+
+    embedding_file_path = os.path.join("Data/embeds.npy")
     coordinator = Coordinator(
-        init_no_agents=5,
+        init_no_agents=2,
         init_dp_per_agent=2,
         save_output_interval="00:01:00",
         communication_interval="00:01:00",
         sliding_window_interval="00:01:00",
         assign_radius=0.80,
         outlier_threshold=0.83,
-        no_topics=10,
-        no_keywords=5,
+        no_topics=2,
+        no_keywords=2,
         agent_fading_rate=0.5,
         delete_agent_weight_threshold=0.4,
         generic_distance=get_distance_cosine,
